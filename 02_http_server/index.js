f85c6b372aed9c2b85a20d39c57dbc72efbff66b
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-04-28 15:50:01 -0600

    http server done

--- 02_http_server/index.js
+++ 02_http_server/index.js
@@ -1,23 +1,48 @@
+const { read } = require('fs');
 const http = require('http');
 
 const PORT = 3000;
 
 const server = http.createServer();
 
-server.once('request', (req, res) => {
-	if (req.url === '/friends') {
+const friends = [
+	{
+		id: 0,
+		name: 'Nikola Tesla',
+	},
+	{
+		id: 1,
+		name: 'Sir Isaac Newton',
+	},
+	{
+		id: 2,
+		name: 'Albert Einstein',
+	},
+];
+
+server.on('request', (req, res) => {
+	const items = req.url.split('/');
+	//  /friends/2 => ['', 'friends', '2']
+	if (req.method === 'POST' && items[1] === 'friends') {
+		req.on('data', (data) => {
+			const friend = data.toString();
+			console.log('Request:', friend);
+			friends.push(JSON.parse(friend));
+		});
+		req.pipe(res);
+	} else if (req.method === 'GET' && items[1] === 'friends') {
 		/* res.writeHead(200, {
 			'Content-Type': 'text/plain',
 		}); */
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
-		res.end(
-			JSON.stringify({
-				id: 1,
-				name: 'Sir Isaac Newton',
-			})
-		);
-	} else if (req.url === '/messages') {
+		if (items.length === 3) {
+			const friendsIndex = Number(items[2]);
+			res.end(JSON.stringify(friends[friendsIndex]));
+		} else {
+			res.end(JSON.stringify(friends));
+		}
+	} else if (req.method === 'GET' && items[1] === 'messages') {
 		res.setHeader('Content-Type', 'text/html');
 		res.write('<html>');
 		res.write('<body>');
