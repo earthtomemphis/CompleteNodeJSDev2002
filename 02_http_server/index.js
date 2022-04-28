ebfc6ca2526518be91a756027675cbd42fff0111
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-04-27 19:30:08 -0600

    http routes

--- 02_http_server/index.js
+++ 02_http_server/index.js
@@ -2,16 +2,36 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain',
-	});
-	res.end(
-		JSON.stringify({
-			id: 1,
-			name: 'Sir Isaac Newton',
-		})
-	);
+const server = http.createServer();
+
+server.once('request', (req, res) => {
+	if (req.url === '/friends') {
+		/* res.writeHead(200, {
+			'Content-Type': 'text/plain',
+		}); */
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(
+			JSON.stringify({
+				id: 1,
+				name: 'Sir Isaac Newton',
+			})
+		);
+	} else if (req.url === '/messages') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<body>');
+		res.write('<ul>');
+		res.write('<li>Hello Isaac</li>');
+		res.write('<li>What are your thoughts on astronomy?</li>');
+		res.write('</ul>');
+		res.write('</body>');
+		res.write('</html>');
+		res.end();
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
 });
 
 server.listen(3000, () => {
