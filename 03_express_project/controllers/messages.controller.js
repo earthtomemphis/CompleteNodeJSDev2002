ae386e50b96617159ecdd5356ac9d7091ee8a724
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-04-29 18:49:07 -0600

    express example complete

---
+++ 03_express_project/controllers/messages.controller.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+function getMessages(req, res) {
+	res.render('messages', {
+		title: 'Messages to my friends!',
+		friend: 'Elon Musk',
+	});
+	/* res.sendFile(
+		path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg')
+	); */
+	// res.send('<ul><li>Helloo Albert!</li></ul>');
+}
+
+function postMessage(req, res) {
+	console.log('Updating message...');
+}
+
+module.exports = {
+	getMessages,
+	postMessage,
+};
